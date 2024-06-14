bb547822414a36c5c775559fa93a29ce5cd0da5a
viny.tito <viny.palazuelos@gmail.com>
2024-06-14 13:40:51 -0700

    agreagamos las funciones de agregar productos

--- Js/conexionAPI.js
+++ Js/conexionAPI.js
@@ -8,8 +8,24 @@ async function listarProductos (){
     
 }
 
+async function enviarProducto (nombre,precio,imagen){
+    const conexion = await fetch('http://localhost:3001/Productos' , {
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            nombre:nombre,
+            precio:precio,
+            imagen:imagen
+        })
+    })
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+
+}
+
 export const conexionAPI = {
-    listarProductos
+    listarProductos , enviarProducto
 }
 
 listarProductos()
